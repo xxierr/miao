3a5208cedec1ba1e2c00f2690d65e53ee325e4da
xierun <xierun@qq.com>
2023-05-30 19:53:09 +0800

    类

--- oop.js
+++ oop.js
@@ -20,3 +20,41 @@ class MySet{
     return this.ele.length
   }
 }
+
+class MyMap{
+  #key = []
+  #value = []
+
+  set(k,v){
+    var kidx = this.#key.indexOf(k)
+    if(kidx >= 0) this.#value[kidx] = v
+    else {
+      this.#key.push(k)
+      this.#value.push(v)
+    }
+  }
+
+  get(k){
+    var kidx = this.#key.indexOf(k)
+    if(kidx >= 0) return this.#value[kidx]
+  }
+
+  has(v){
+    if(this.#key.includes(k)) return true
+    else return false
+  }
+
+  delete(k){
+    var kidx = this.#key.indexOf(k)
+    if(kidx >= 0){
+      this.#key.splice(kidx,1)
+      this.#value.splice(kidx,1)
+      return true
+    }
+    return false
+  }
+
+  size(){
+    return this.#key.length
+  }
+}
