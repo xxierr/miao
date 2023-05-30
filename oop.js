918f7714875f920e5afdde7f6eaa06be38908e36
xierun <xierun@qq.com>
2023-05-30 20:53:22 +0800

    类

--- oop.js
+++ oop.js
@@ -82,3 +82,127 @@ class Vector{
   }
 }
 
+class Complex{
+  constructor(re,im){
+    this.re = re
+    this.im = im
+  }
+
+  plus(c){
+    return new Complex(this.re + c.re , this.im + c.im)
+  }
+
+  minus(c){
+    return new Complex(this.re - c.re , this.im - c.im)
+  }
+
+  mul(c){
+    var re = this.re * c.re - this.im * c.im
+    var im = this.re * c.im + this.im * c.re
+    return new Complex(re,im)
+  }
+  
+  div(c){
+    var hper = new Complex(this.re, -this.im)
+    var fenzi = this.mul(hper)
+    var fenmu = c.mul(hper)
+    var re = fenzi.re / fenmu.re
+    var im = fenmu.im / fenmu.re
+    return new Complex(re,im)
+  }
+}
+
+class Stack{
+  head = null
+  len = 0
+  push(val){
+    var node = {val, next: null}
+    len++
+    if(this.head == null){
+      this.head = node
+      return
+    }else {
+      node.next = this.head
+      this.head = node
+      return
+    }
+  }
+
+  pop(){
+    var node = {val, next: null}
+    if(this.head == null) return undefined
+    len--
+    var res = this.head.val
+    this.head = this.head.next
+    return res
+  }
+
+  size(){
+    return this.len
+  }
+}
+
+class Queue{
+  vals = []
+
+  add(val){
+    this.vals.push(val)
+  }
+
+  pop(){
+    return this.val.shift()
+  }
+
+  size(){
+    return this.vals.length
+  }
+}
+
+class LinkedList{
+  head = null
+  tail = null
+
+  append(val){
+    var node = {val, next: null}
+    if(this.head == null){
+      this.head = this.tail = node
+      return
+    }else {
+      this.tail.next = node
+      this.tail = node
+      return
+    }
+  }
+
+  prepend(val){
+    var node = {val, next: null}
+    if(this.head == null){
+      this.head = this.tail = node
+      return
+    }else {
+      node.next = this.head
+      this.head = node
+      return
+    }
+  }
+
+  at(idx){
+    var p = this.head
+    var c = 0
+    while(c<idx){
+      p = p.next
+      c++
+    }
+    return p
+  }
+
+  size(){
+    var p = this.head
+    var len = 0
+    while(p){
+      len++
+      p = p.next
+    }
+    return len
+  }
+}
